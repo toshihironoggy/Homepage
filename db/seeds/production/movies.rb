a3195edaaa54eb663dac41a186c717923232e34c
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-02-22 05:46:51 +0000

    change seed data

--- db/seeds/production/movies.rb
+++ db/seeds/production/movies.rb
@@ -1,8 +1,10 @@
 Movie.create(
   title: 'H29MT1大問1(1)',
-  url: 'H29MT1_1_1.MP4'
+  url: 'H29MT1_1_1.MP4',
+  image: 'H29MT1_1_1.JPG'
 )
 Movie.create(
   title: 'H29MT1大問1(2)',
-  url: 'H29MT1_1_2.MP4'
+  url: 'H29MT1_1_2.MP4',
+  image: 'H29MT1_1_2.JPG'
 )
