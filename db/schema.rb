b540bb8742e8f3cdf30dee909bc91d14c69b2552
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-02-24 04:14:19 +0000

    add tag_name to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_035452) do
+ActiveRecord::Schema.define(version: 2020_02_24_033417) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_02_22_035452) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "tag_name"
   end
 
   create_table "schools", force: :cascade do |t|
--- db/seeds/development/movies.rb
+++ db/seeds/development/movies.rb
@@ -1,10 +1,12 @@
 Movie.create(
   title: 'H29MT1大問1(1)',
   url: 'H29MT1_1_1.MP4',
-  image: 'H29MT1_1_1.JPG'
+  image: 'H29MT1_1_1.JPG',
+  tag_name: '中1,正負の数'
 )
 Movie.create(
   title: 'H29MT1大問1(2)',
   url: 'H29MT1_1_2.MP4',
-  image: 'H29MT1_1_2.JPG'
+  image: 'H29MT1_1_2.JPG',
+  tag_name: '中2,文字式'
 )
