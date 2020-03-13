11748f85bd96e2320f2a7a99ebd8684d68207591
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-03-13 14:50:18 +0000

    change top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,23 @@
 Rails.application.routes.draw do
   
-  get 'schools/index'
-  get 'employees/index'
-  get "users/index"
-  
-  #root 'home#top'
-  #root 'employees#login_form'
+  root 'home#top'
   
   # devise_for :users
   get "pages/index" => "pages#index"
   get "pages/show" => "pages#show"
-
+  
+  get 'schools/index'
+  get "users/index"
   get "testkai/index" => "testkai#index"
   get "testkai/new" => "testkai#new"
   post "testkai/create" => "testkai#create"
 
-  #get "login" => "employees#login_form"
+  get "login" => "employees#login_form"
   post "login" => "employees#login"
   post "logout" => "employees#logout"
 
-  
-  root "movies#index"
   resources :movies
-  # get "movies/:id" => "movies#show"
+  resources :employees
   
   
 
