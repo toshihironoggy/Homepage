2a9972b26c2e4fa07f544437c2a161f07784c763
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-03-15 04:52:22 +0000

    add index btn

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,21 @@ Rails.application.routes.draw do
   get "pages/index" => "pages#index"
   get "pages/show" => "pages#show"
   
+  #school-----------------------
   get 'schools/index'
+  
   get "users/index"
+  
   get "testkai/index" => "testkai#index"
   get "testkai/new" => "testkai#new"
   post "testkai/create" => "testkai#create"
-
+  
+  resources :employees
   get "login" => "employees#login_form"
   post "login" => "employees#login"
   post "logout" => "employees#logout"
 
   resources :movies
-  resources :employees
   
   
 
