31905dbec6f875c5eaac4cf478ce0e0b6f1c9a06
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-03-19 14:00:55 +0000

    before destroy controller testkai

--- test/controllers/employees_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EmployeesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get employees_index_url
-    assert_response :success
-  end
-
-end
