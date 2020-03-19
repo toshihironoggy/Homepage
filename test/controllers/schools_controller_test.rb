51e5bf46e3e6a79597f4bbf70fcbd92b02be16c9
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-03-19 13:51:59 +0000

    before destroy employees controller

--- test/controllers/schools_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SchoolsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get schools_index_url
-    assert_response :success
-  end
-
-end
