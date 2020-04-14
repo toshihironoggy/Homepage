92c931954b5a084a71bb6dca660ca192a75136df
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-04-14 13:16:49 +0000

    change arrows

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,8 +1,8 @@
 
 $(function() {
     $('.a').slick({
-      prevArrow: '<span class="fa fa-angle-left" style="color:red;"></span>',
-      nextArrow: '<span class="fa fa-angle-right" style="color:red;"></span>',
+      dots:true,
+      arrows:true,
       
         });
 });
