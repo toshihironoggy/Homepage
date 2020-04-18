bc5af6651378f69c4be44b569a510202ed76fcb2
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2020-04-18 06:24:09 +0000

    merge complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,17 @@ class HomeController < ApplicationController
   PER=5
   
   def index
-    if params[:search_tag]
+    if params[:search_tag] 
       @movies = Movie.where("tag_name LIKE ?","%#{params[:search_tag]}%").page(params[:page]).per(PER)
     else 
       @movies = Movie.all.page(params[:page]).per(PER)
     end
     
-    if params[:search_slide_tag]
+    if params[:search_slide_tag] 
       @slides = Slide.where("tag_name LIKE ?","%#{params[:search_slide_tag]}%").page(params[:page]).per(PER)
-    else 
+    else
       @slides = Slide.all.page(params[:page]).per(PER)
-    end
-    
+    end 
   end
 
   def show
