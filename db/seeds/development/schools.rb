190b2e89f9c888d7fe77e024bc8c0bed7db20e7e
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2021-02-28 07:50:04 +0000

    modify school lat and lng

--- db/seeds/development/schools.rb
+++ db/seeds/development/schools.rb
@@ -1,13 +1,13 @@
 School.create(
   schoolname: "城南学園",
   address: "大阪市東住吉区照ヶ丘矢田2丁目14-10",
-  latitude: 34.60828,
-  longitude: 135.5359483
+  latitude: 34.607923,
+  longitude: 135.537971
   
 )
 School.create(
   schoolname: "大阪桐蔭",
   address: "大阪府大東市中垣内3丁目1-1",
-  latitude: 34.7081431,
-  longitude: 135.6454219
+  latitude: 34.707958,
+  longitude: 135.645655
 )
