9eb04848687aede1b4ec2decc2a50db808192f01
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2021-03-15 13:17:23 +0000

    change seeds data

--- db/seeds/development/schools.rb
+++ db/seeds/development/schools.rb
@@ -3,8 +3,8 @@ School.create(
   address: "大阪市東住吉区照ヶ丘矢田2丁目14-10",
   latitude: 34.607923,
   longitude: 135.537971,
-  max_num_seats: 100,
-  little_left_seats: 20
+  max_num_seats: 20,
+  little_left_seats: 10
   
 )
 School.create(
@@ -12,6 +12,6 @@ School.create(
   address: "大阪府大東市中垣内3丁目1-1",
   latitude: 34.707958,
   longitude: 135.645655,
-  max_num_seats: 200,
-  little_left_seats: 40
+  max_num_seats: 40,
+  little_left_seats: 20
 )
--- db/seeds/production/schools.rb
+++ db/seeds/production/schools.rb
@@ -2,12 +2,16 @@ School.create(
   schoolname: "城南学園",
   address: "大阪市東住吉区照ヶ丘矢田2丁目14-10",
   latitude: 34.607923,
-  longitude: 135.537971
+  longitude: 135.537971,
+  max_num_seats: 20,
+  little_left_seats: 10
   
 )
 School.create(
   schoolname: "大阪桐蔭",
   address: "大阪府大東市中垣内3丁目1-1",
   latitude: 34.707958,
-  longitude: 135.645655
+  longitude: 135.645655,
+  max_num_seats: 40,
+  little_left_seats: 20
 )
