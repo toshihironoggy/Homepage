8cf5ebaddcf6b654b4f604b37a4cec159fae9810
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2021-03-16 13:24:59 +0000

    can not buy when seat=0

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,9 +6,15 @@ class MapsController < ApplicationController
   
   def update
 	  @school= School.find(params[:school_id])
-	  @school.max_num_seats = [@school.max_num_seats, (params[:number].to_i)*(-1)].sum
-	  @school.save
-	  redirect_to root_path
+    @school.max_num_seats = [@school.max_num_seats, (params[:number].to_i)*(-1)].sum
+    if @school.max_num_seats >= 0 then
+	    @school.save
+	    redirect_to root_path
+    else
+      flash[:alert] ="席数が足りません。他の会場を選んでyo"
+      redirect_to root_path
+    end
+   
   end
   
 end
--- db/seeds/development/schools.rb
+++ db/seeds/development/schools.rb
@@ -5,7 +5,6 @@ School.create(
   longitude: 135.537971,
   max_num_seats: 20,
   little_left_seats: 10
-  
 )
 School.create(
   schoolname: "大阪桐蔭",
@@ -13,5 +12,21 @@ School.create(
   latitude: 34.707958,
   longitude: 135.645655,
   max_num_seats: 40,
+  little_left_seats: 20
+)
+School.create(
+  schoolname: "清明学院",
+  address: "大阪市住吉区墨江2丁目4番4号",
+  latitude: 34.607612 ,
+  longitude: 135.49425,
+  max_num_seats: 40,
+  little_left_seats: 20
+)
+School.create(
+  schoolname: "清風",
+  address: "大阪府大阪市天王寺区石ケ辻町12-16",
+  latitude: 34.663226 ,
+  longitude: 135.521651,
+  max_num_seats: 100,
   little_left_seats: 20
 )
