63f7a9158368e4360a33cb0cb6780cac4d82e0b1
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2021-10-14 14:02:00 +0000

    we can tags

--- app/controllers/mogisupple_controller.rb
+++ app/controllers/mogisupple_controller.rb
@@ -4,9 +4,14 @@ class MogisuppleController < ApplicationController
   def index
     if params[:search_tag]
       @movies = Movie.where("tag_name LIKE ?","%#{params[:search_tag]}%")
+      if user_signed_in?
+        redirect_to  controller: :users, action: :show
+      end
     else 
       @movies = Movie.all
     end
+    
+    
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
+    @user = User.find(current_user.id)
     
     if params[:search_tag]
       @movies = Movie.where("tag_name LIKE ?","%#{params[:search_tag]}%")
