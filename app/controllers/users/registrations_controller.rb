b94e3d72f0b0cd8f7feffc3a52580adb53fc991c
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2021-10-16 07:08:24 +0000

    we can tag completely at show

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -71,7 +71,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
      devise_parameter_sanitizer.permit(:account_update, keys: [:email])
-     #devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
      devise_parameter_sanitizer.permit(:account_update, keys: [:password])
      devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
