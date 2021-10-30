882d40ab252f0d632d4abd68590eb275c0fb76b5
EC2 Default User <ec2-user@ip-172-31-37-228.ap-northeast-1.compute.internal>
2021-10-30 12:31:35 +0000

    change top-page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #root to: 'maps#index'
   #root to: 'shops#index'
   
-  root to: 'mogisupple#index'
+  
   
   get 'maps/index' =>'maps#index'
   #↓これがないとNo routes match [POST] "/"がでる
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
   
+  #deviseのログイン画面をrootに設定
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
+  
   get 'pages/show'
   
   #root "home#index"
@@ -32,23 +37,6 @@ Rails.application.routes.draw do
   
   #resources :mogisupple
    
-  #get "testkai_start/new" => "testkai_start#new" 
-  #get "testkai_start/index" => "testkai_start#index"
-  #post "testkai_start/create" => "testkai_start#create"
-  
-  #get "/login" => "testkai_start_login#login_form" 
-  #post "/login" => "testkai_start_login#login" 
-  #post "logout" => "testkai_start_login#login_form" 
-  
-  #get 'testkai_end_login/login_form' => 'testkai_end_login#login_form' 
-  #post 'testkai_end_login/login' => 'testkai_end_login#login' 
-  #post 'testkai_end_login/logout' => 'testkai_end_login#login_form' 
-  
-  #get 'testkai_end/index' => "testkai_end#index"
-  #get 'testkai_end/new' => "testkai_end#new"
-  #post 'testkai_end/create' => "testkai_end#create"
-  
-  #resources :employee
   resources :users, :only => [:show]
   
   resources :events
